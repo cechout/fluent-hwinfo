7f64a80cf19d9f168611c6a1b2758f7a59442a2c
cechout <daniel.j.cech@gmail.com>
2026-07-15 00:25:18 +0200

    refactor: extract LiveChart to a UserControl (#6)

---
+++ FluentHwInfo/Views/Graph.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace FluentHwInfo.Views
+{
+    public sealed partial class Graph : UserControl
+    {
+        public Graph()
+        {
+            InitializeComponent();
+        }
+    }
+}
